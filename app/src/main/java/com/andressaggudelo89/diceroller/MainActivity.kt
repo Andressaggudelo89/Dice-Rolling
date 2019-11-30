75a3d5d5ecfbc72420eab953f5f2b65dc9949ff8
andressaggududelo89 <cagudeloa@uninorte.edu.co>
2019-11-29 20:27:17 -0500

    Good practise implemented. Not calling same object each time Button is pressed. Solved using 'lateinit'

--- app/src/main/java/com/andressaggudelo89/diceroller/MainActivity.kt
+++ app/src/main/java/com/andressaggudelo89/diceroller/MainActivity.kt
@@ -10,9 +10,13 @@ import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
@@ -29,7 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(selectedImage)
     }
 }
